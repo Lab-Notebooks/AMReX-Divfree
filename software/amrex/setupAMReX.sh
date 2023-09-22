1b727a0cf92d20a31f4f37bc960ea3f182e0730d
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-09-22 14:34:12 -0500

    add tests and bittree library

--- software/amrex/setupAMReX.sh
+++ software/amrex/setupAMReX.sh
@@ -12,12 +12,14 @@ fi
 
 # configure and install amrex in 2D
 make clean
-./configure --dim=2 --prefix=$AMREX2D_HOME --enable-tiny-profile=yes --debug=no
+BITTREE_HOME=$BITTREE_2D_HOME
+./configure --dim=2 --prefix=$AMREX2D_HOME --enable-bittree=yes --enable-tiny-profile=yes --debug=no
 make -j
 make install
 
 # configure and install amrex in 3D
 make clean
-./configure --dim=3 --prefix=$AMREX3D_HOME --enable-tiny-profile=yes --debug=no
+BITTREE_HOME=$BITTREE_3D_HOME
+./configure --dim=3 --prefix=$AMREX3D_HOME --enable-bittree=yes --enable-tiny-profile=yes --debug=no
 make -j
 make install
