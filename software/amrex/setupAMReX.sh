fa991d8b0bb797c10372f564ebb2eb833d9e59d1
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-10-04 11:35:21 -0500

    update versions

--- software/amrex/setupAMReX.sh
+++ software/amrex/setupAMReX.sh
@@ -2,21 +2,21 @@
 
 # Setup AMReX
 if [ ! -d "AMReX" ]; then
-	git clone git@github.com:Box-Tools/amrex.git --branch bt-merge AMReX && cd AMReX
+	git clone git@github.com:AMReX-codes/amrex.git --branch development AMReX && cd AMReX
 else
 	cd AMReX
 fi
 
 # configure and install amrex in 2D
-make clean
+make clean || true
 BITTREE_HOME=$BITTREE_2D_HOME
-./configure --dim=2 --prefix=$AMREX2D_HOME --enable-bittree=yes --enable-tiny-profile=yes --debug=no
+./configure --dim=2 --prefix=$AMREX2D_HOME --enable-bittree=no --enable-tiny-profile=yes --debug=no
 make -j
 make install
 
 # configure and install amrex in 3D
-make clean
+make clean || true
 BITTREE_HOME=$BITTREE_3D_HOME
-./configure --dim=3 --prefix=$AMREX3D_HOME --enable-bittree=yes --enable-tiny-profile=yes --debug=no
+./configure --dim=3 --prefix=$AMREX3D_HOME --enable-bittree=no --enable-tiny-profile=yes --debug=no
 make -j
 make install
