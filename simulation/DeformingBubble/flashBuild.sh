4f035bc40c635d51a8547cc023c45cf6504da3ea
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-10-26 14:56:28 -0500

    clean up and update

--- simulation/DeformingBubble/flashBuild.sh
+++ simulation/DeformingBubble/flashBuild.sh
@@ -1,14 +1,8 @@
 # cache the value of current working directory
 NodeDir=$(realpath .)
 
-ExternalSimName="incompFlow/FlashExternal"
-
-# Link private simulation directory
-rm -r $FLASHX_HOME/source/Simulation/SimulationMain/$ExternalSimName
-ln -s $NodeDir $FLASHX_HOME/source/Simulation/SimulationMain/$ExternalSimName 
-
 # run Flash-X setup
-cd $FLASHX_HOME && ./setup $ExternalSimName $FlashOptions
+cd $FLASHX_HOME && ./setup $FlashOptions
 
 # compile the simulation and copy files
 cd $FLASHX_HOME/object && make && cp flashx $NodeDir/
