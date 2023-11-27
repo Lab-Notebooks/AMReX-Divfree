2310ca62dc87e1ddf1b5652964e81e9f4778857a
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-11-26 18:47:52 -0600

    Update flashOptions.sh

--- simulation/DeformingBubble/amrex/flashOptions.sh
+++ simulation/DeformingBubble/amrex/flashOptions.sh
@@ -1,3 +1,3 @@
 # set Flash-X options
 FlashOptions="incompFlow/DeformingBubble -auto -2d -nxb=16 -nyb=16 +amrex \
-              -maxblocks=150 -site=$SiteHome +parallelIO IOWriteGridFiles=TRUE"
+              -maxblocks=150 -site=$SiteHome +parallelIO +incomp"
